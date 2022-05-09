9afcde856693fb108e6a18473b6a86b70536206d
federuiz <federicoruiz@sirius.com.ar>
2022-05-09 14:56:37 -0300

    Initial Commit

---
+++ LeanSdk/LeanSdk.h
@@ -0,0 +1,18 @@
+//
+//  LeanSdk.h
+//  LeanSdk
+//
+//  Created by Fede Ruiz on 09/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeanSdk.
+FOUNDATION_EXPORT double LeanSdkVersionNumber;
+
+//! Project version string for LeanSdk.
+FOUNDATION_EXPORT const unsigned char LeanSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeanSdk/PublicHeader.h>
+
+
